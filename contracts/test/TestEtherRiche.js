06a77ebb0c1aa3fd2f15f9fb5d11811b8a007cef
Stephen Holstein <sholstei@yahoo.com>
2017-06-29 03:33:10 -0700

    fixing expectation language

--- contracts/test/TestEtherRiche.js
+++ contracts/test/TestEtherRiche.js
@@ -118,7 +118,7 @@ describe( 'etherRiche', function ()
     // TODO
   } );
 
-  it( 'should require the degrade all claims in 30 days', function ()
+  it( 'should degrade all claims in 30 days', function ()
   {
     // TODO
   } );
